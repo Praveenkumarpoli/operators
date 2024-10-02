f91c09ebe68e96ea18c7d1598696574ea8422faf
PoliPavan <polipavan123@gmail.com>
2024-10-02 12:16:37 +0530

    operators

---
+++ jh.js
@@ -0,0 +1,33 @@
+// var a=10;
+// var b= a++ +a++;
+//  console.log(a + b++);
+
+//  var a=10;
+// var b= a++ +a++;
+//  console.log(a + b++);
+
+// var a = 567;
+// var b = 456;
+// console.log(a&&b);
+
+// var a = 567;
+// var b = 456;
+// console.log(a&&b);
+
+// var a = 2 == "2.0";
+// var b = 3 != 3;
+// console.log(!!!!!!!!!!(((10===20)&&(2.0===2.0)|| !(35<40))))
+// console.log(10===20)
+// var a='"Hello world"'
+// console.log(a+""+'"!"')
+
+// var a=20;
+// var b=20;
+// var c=20;
+// result=(a>b && a>c)? "A is big" :
+//         (b>a && b>c)? "b is big":
+//       (a==b && b==c)? "All are same" : "c id big";
+// console.log(result)
+
+
+
